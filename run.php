6085f7039e5d39e97a1cd59a794775a565848d5b
Boris server <boris.rougagnou@ynov.com>
2019-02-25 00:00:01 +0100

    send mail

--- run.php
+++ run.php
@@ -1,5 +1,20 @@
 <?php
 
+function send_mail($error_code, $server)
+{
+	$headers= 'MIME-Version: 1.0' . '\r\n';
+	$headers .= 'Content-type:text/html;charset=UTF-8' . '\r\n';
+
+	$to = 'trucbidule@grr.la';
+	$subject= 'erreur avec le serveur';
+
+	$body = 'Une erreur est survenue avec le serveur ';
+	$body.= '"' . $server . '". ';
+	$body.= 'code erreur:' . $error_code . '.';
+
+	mail($to, $subject, $body, $headers);
+}
+
 try
 {
 	$dbhost = 'localhost';
@@ -11,6 +26,7 @@ try
 	$tmpurl = "";
 	foreach($connexion->query("SELECT websites.url, status, time FROM websites LEFT JOIN logs ON websites.url = logs.url ORDER BY logs.url DESC, logs.time DESC;") as $row)
 	{
+		send_mail('404', $row['url']);
 		if ($tmpurl != $row['url'])
 			$tmpurl = $row['url'];
 	}
